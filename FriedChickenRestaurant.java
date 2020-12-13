3846aaeed26911b21725a15ba30bb35b884a56fc
Eicplants <75203127+Eicplants@users.noreply.github.com>
2020-12-13 03:34:27 +0000

    Delete FriedChickenRestaurant.java

--- FriedChickenRestaurant.java
+++
@@ -1,6 +0,0 @@
-package q1FriedChicken;
-
-public interface FriedChickenRestaurant {
-public abstract void SellPackage(SetMeal setMeal);//出售套餐
-public abstract void BatchPurchase(Drinks drink,int Number);//批量进货
-}
